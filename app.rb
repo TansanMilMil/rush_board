ebed38e23490346ab86c88db64abc32366bcbccc
tansantktk <tansantktk@hotmail.co.jp>
2019-05-20 19:40:39 +0900

    [update]base

--- app.rb
+++ app.rb
@@ -1,7 +1,60 @@
-#!/usr/bin/env ruby
+require 'sinatra/base'
+require 'sinatra/reloader'
 
-require 'sinatra'
+class App < Sinatra::Base
+  configure :development do
+    Bundler.require :development
+    register Sinatra::Reloader
+  end
 
-get '/' do
-	erb :index
+  get '/' do
+    get_weather_info
+    get_train_info
+    erb :index
+  end
+
+  # 天気情報取得
+  def get_weather_info
+    require 'uri'
+    require 'net/http'
+    require 'yaml'
+    # 大阪の天気取得
+    data = Net::HTTP.get(URI.parse('http://weather.livedoor.com/forecast/webservice/json/v1?city=270000'))
+    forecast_obj = YAML.load(data)
+
+    today_temperature_min = '--'
+    today_temperature_max = '--'
+    tomorrow_temperature_min = '--'
+    tomorrow_temperature_max = '--'
+    unless forecast_obj['forecasts'][0]['temperature']['min'].nil?
+      today_temperature_max = forecast_obj['forecasts'][0]['temperature']['min']['celsius']
+    end
+    unless forecast_obj['forecasts'][0]['temperature']['max'].nil?
+      today_temperature_max = forecast_obj['forecasts'][0]['temperature']['max']['celsius']
+    end
+    unless forecast_obj['forecasts'][1]['temperature']['min'].nil?
+      today_temperature_max = forecast_obj['forecasts'][1]['temperature']['min']['celsius']
+    end
+    unless forecast_obj['forecasts'][1]['temperature']['max'].nil?
+      today_temperature_max = forecast_obj['forecasts'][1]['temperature']['max']['celsius']
+    end
+
+    @today = { 
+      weather_state: forecast_obj['forecasts'][0]['telop'],
+      temperature_min: today_temperature_min,
+      temperature_max: today_temperature_max,
+      image_url: forecast_obj['forecasts'][0]['image']['url']
+    }
+    @tomorrow = {
+      weather_state: forecast_obj['forecasts'][1]['telop'],
+      temperature_min: tomorrow_temperature_min,
+      temperature_max: tomorrow_temperature_max,
+      image_url: forecast_obj['forecasts'][1]['image']['url']
+    }
+  end
+
+  # 鉄道情報取得
+  def get_train_info
+
+  end
 end
